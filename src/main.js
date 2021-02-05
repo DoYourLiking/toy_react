e201aef27fd3312b69b6f8c3fc20b4d33c4cec96
DoYourLiking <linus_zk@163.com>
2021-02-05 17:00:14 +0800

    feat: 完成react setState

--- src/main.js
+++ src/main.js
@@ -1,7 +1,29 @@
-import {render, zk_creatElement,Component} from "./react"
-class MyCon extends Component{
-  
-    render() { return <div><h1>这是一个组件函数</h1>{this.childRen}</div>; }
+import { render, zk_creatElement, Component } from "./react";
+class MyCon extends Component {
+  constructor() {
+    super();
+    this.state = {
+      a: 1,
+      b: 2,
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>这是一个组件函数</h1>
+        <h2>{this.state.a.toString()}</h2>
+        <h2>{this.state.b.toString()}</h2>
+        <button
+          onclick={() => {
+            this.setState({ a: this.state.a+1 });
+          }}
+        >
+          add
+        </button>
+        {this.childRen}
+      </div>
+    );
+  }
 }
 // var a = zk_creatElement("div", {
 //     id: "root",
@@ -15,8 +37,9 @@ class MyCon extends Component{
 //   }));
 // let a = <div id="root" class="root">222<div name="child"></div><div name="child"></div><div
 // a由babel后会变成上图的形式，会自动对子节点递归进行creatElement函数，因此只需要定义一个createElement函数即可。此函数只需要最终最终能将属性添加到dom上并且，能够将子节点也添加进去即可（无需递归）。
-render (
-    
-        <MyCon name="main" class="my-con"><div>child 1</div></MyCon>
-    ,document.body
+render(
+  <MyCon name="main" class="my-con">
+    <div>child 1</div>
+  </MyCon>,
+  document.body
 );
--- src/react.js
+++ src/react.js
@@ -1,71 +1,120 @@
+const RENDER_TO_DOM = Symbol("render");
 class ElementWrapper {
-    constructor(type) {
-        this.dom = document.createElement(type)
+  constructor(type) {
+    this.dom = document.createElement(type);
+  }
+  setAttribute(key, value) {
+    if (key.match(/^on([\s\S]+)$/)) {
+      this.dom.addEventListener(
+        RegExp.$1.replace(/^[\s\S]/, (c) => c.toLocaleLowerCase()),
+        value,
+        false
+      );
+    } else {
+      this.dom.setAttribute(key, value);
     }
-    setAttribute(key, value) {
-        this.dom.setAttribute(key, value)
-    }
-    appendChild(child) {
-        this.dom.appendChild(child.dom)
-    }
+  }
+  appendChild(child) {
+    let range = document.createRange();
+    range.setStart(this.dom, this.dom.childNodes.length);
+    range.setEnd(this.dom, this.dom.childNodes.length);
+    child[RENDER_TO_DOM](range);
+  }
+  [RENDER_TO_DOM](range) {
+    range.deleteContents();
+    range.insertNode(this.dom);
+  }
 }
 class TextWrapper {
-    constructor(content) {
-        this.dom = document.createTextNode(content)
-    }
-
+  constructor(content) {
+    this.dom = document.createTextNode(content);
+  }
+  [RENDER_TO_DOM](range) {
+    range.deleteContents();
+    range.insertNode(this.dom);
+  }
 }
 export class Component {
-    constructor(type) {
-        this.props = Object.create(null)
-        this.childRen = []
-        this._dom = null
+  constructor(type) {
+    this.props = Object.create(null);
+    this.childRen = [];
+    this._dom = null;
+    this._range = null;
+  }
+  setAttribute(key, value) {
+    this.props[key] = value;
+  }
+  appendChild(child) {
+    this.childRen.push(child);
+  }
+  [RENDER_TO_DOM](range) {
+    this._range = range;
+    this.render()[RENDER_TO_DOM](range);
+  }
+  reRender() {
+    this._range.deleteContents();
+    this[RENDER_TO_DOM](this._range);
+  }
+  setState(new_state) {
+    if (this.state == null || typeof this.state !== "object") {
+      this.state = new_state;
+      this.reRender();
+      return;
     }
-    setAttribute(key, value) {
-        this.props[key] = value
-    }
-    appendChild(child) {
-        this.childRen.push(child)
-    }
-    get dom() {
-        if (!this._dom) {
-            this._dom = this.render().dom
+    let merge = function (old_state, new_state) {
+      for (let i in new_state) {
+        if (old_state[i] !== null || typeof old_state[i] !== "object") {
+          old_state[i] = new_state[i];
+        } else {
+          merge(old_state[i], new_state[i]);
         }
-        return this._dom//不能添加else,拿到以后必须返回dom
+      }
+    };
+    merge(this.state, new_state);
+    this.reRender();
+  }
+  // get dom() {
+  //     if (!this._dom) {
+  //         this._dom = this.render().dom
+  //     }
+  //     return this._dom不能添加else,拿到以后必须返回dom,改为将render_to_dom方法写到textWrapper和textWrapper
 
-    }
+  // }
 }
 export function zk_creatElement(tagName, options, ...childRen) {
-    let e
-    if (typeof tagName === "string") {
-        e = new ElementWrapper(tagName)
-    } else {
-        e = new tagName
+  let e;
+  if (typeof tagName === "string") {
+    e = new ElementWrapper(tagName);
+  } else {
+    e = new tagName();
+  }
+  for (let i in options) {
+    e.setAttribute(i, options[i]);
+  }
+  // for(let i of childRen){
+  //     if(typeof i=="string"){
+  //         i=new TextWrapper(i)
+  //     }
+  //     e.appendChild(i)
+  // }未考虑数组的情况,即<MyCon><div></div></MyCon>,最后{this.children}，此时jsx会把this.children收集到当前的children数组，会出现就会出现appendChild(child)的情况。
+  let insert = (children) => {
+    for (let i of children) {
+      if (typeof i == "string") {
+        i = new TextWrapper(i);
+      }
+      if (Array.isArray(i)) {
+        insert(i);
+      } else {
+        e.appendChild(i);
+      }
     }
-    for (let i in options) {
-        e.setAttribute(i, options[i])
-    }
-    // for(let i of childRen){
-    //     if(typeof i=="string"){
-    //         i=new TextWrapper(i)
-    //     }
-    //     e.appendChild(i)
-    // }未考虑数组的情况,即<MyCon><div></div></MyCon>,最后{this.children}，此时jsx会把this.children收集到当前的children数组，会出现就会出现appendChild(child)的情况。
-    let insert = (children) => {
-        for (let i of children) {
-            if (typeof i == "string") {
-                i = new TextWrapper(i)
-            }
-            if (Array.isArray(i)) {
-                insert(i)
-            }
-            else{e.appendChild(i)}
-        }
-    }
-    debugger
-    insert(childRen)
-    return e;
+  };
+  insert(childRen);
+  return e;
 }
 export function render(component, parentDom) {
-    parentDom.appendChild(component.dom)
+  let range = document.createRange();
+  range.setStart(parentDom, 0);
+  range.setEnd(parentDom, parentDom.childNodes.length);
+  component[RENDER_TO_DOM](range);
 }
